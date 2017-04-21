71e783799bcf550c490beeee083db5000c5565b5
Lvshengjie <285858315@qq.com>
2017-04-21 16:45:55 +0800

    init

---
+++ src/components/app/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import 'normalize.css/normalize.css'
+import './index.scss'
+
+export default {
+    name: 'app',
+}
+
+</script>
